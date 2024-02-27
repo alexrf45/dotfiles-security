30dfa748c29d39c1ae3383c7f6a9de1995a63a47
alexrf45 <fonalex45@gmail.com>
2024-02-26 22:10:36 -0500

    kanagawa

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,30 +1,12 @@
--- return {
---   -- add gruvbox
---   --{ "kdheepak/monochrome.nvim" },
---   --{ "audibleblink/hackthebox.vim" },
---   { "Mofiqul/dracula.nvim" },
---   -- Configure LazyVim to load gruvbox
---   {
---     "LazyVim/LazyVim",
---     opts = {
---       theme = "dracula",
---       colorscheme = "dracula",
---       background = "dark",
---       transparent = true,
---     },
---   },
--- }
-
 return {
   -- add dracula
-  { "Mofiqul/dracula.nvim" },
+  { "rebelot/kanagawa.nvim" },
 
   -- Configure LazyVim to load dracula
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "dracula",
-      transparent_bg = true,
+      colorscheme = "kanagawa-dragon",
     },
   },
 }
