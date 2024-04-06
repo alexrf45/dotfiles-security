74a9fef426b029094b2e08e79ff44dcf6a26a558
alexrf45 <fonalex45@gmail.com>
2024-04-05 21:25:35 -0400

    theme change again

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,7 +1,7 @@
 return {
-  "dracula/vim",
+  "rebelot/kanagawa.nvim",
   priority = 100,
   config = function()
-    vim.cmd([[colorscheme dracula]])
+    vim.cmd([[colorscheme kanagawa-dragon]])
   end,
 }
