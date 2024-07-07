721df5f2ea61c712979387590980b98d8c33d045
alexrf45 <fonalex45@gmail.com>
2024-07-06 22:49:50 -0400

    htb again

--- .config/nvim/lua/plugins/color.lua
+++ .config/nvim/lua/plugins/color.lua
@@ -8,13 +8,8 @@
 -- }
 
 return {
-  "sainnhe/gruvbox-material",
-  lazy = false,
-  priority = 1000,
+  "audibleblink/hackthebox.vim",
   config = function()
-    -- Optionally configure and load the colorscheme
-    -- directly inside the plugin declaration.
-    vim.g.gruvbox_material_enable_italic = true
-    vim.cmd.colorscheme("gruvbox-material")
+    vim.cmd([[colorscheme hackthebox]])
   end,
 }
