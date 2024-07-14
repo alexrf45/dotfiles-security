2bb4378ad6a8001ee48bcdba50bb213573494d9f
alexrf45 <fonalex45@gmail.com>
2024-07-14 02:39:15 -0400

    more themes

--- .config/nvim/lua/plugins/color.lua
+++ .config/nvim/lua/plugins/color.lua
@@ -51,13 +51,13 @@
 -- }
 
 return {
-  "savq/melange-nvim",
+  "RRethy/base16-nvim",
   lazy = false,
   priority = 1000,
   config = function()
     -- Optionally configure and load the colorscheme
     -- directly inside the plugin declaration.
     vim.g.edge_enable_italic = true
-    vim.cmd.colorscheme("melange")
+    vim.cmd.colorscheme("base16-irblack")
   end,
 }
