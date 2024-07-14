4e1e3b8591c3d193974e67ec31e5c936d5b2301a
alexrf45 <fonalex45@gmail.com>
2024-07-14 14:17:13 -0400

    eighties theme

--- .config/nvim/lua/plugins/color.lua
+++ .config/nvim/lua/plugins/color.lua
@@ -58,6 +58,6 @@ return {
     -- Optionally configure and load the colorscheme
     -- directly inside the plugin declaration.
     vim.g.edge_enable_italic = true
-    vim.cmd.colorscheme("base16-irblack")
+    vim.cmd.colorscheme("base16-eighties")
   end,
 }
