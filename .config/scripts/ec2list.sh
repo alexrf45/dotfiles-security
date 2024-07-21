69ffaa5a72dc3297eb16356e5fd41d57ab28e421
alexrf45 <fonalex45@gmail.com>
2024-07-20 21:06:50 -0400

    move configs to bootstrap

--- .config/scripts/ec2list.sh
+++
@@ -1,4 +0,0 @@
-aws ec2 describe-instances \
-    --filters Name=tag-key,Values=Name \
-    --query 'Reservations[*].Instances[*].{Instance:InstanceId,AZ:Placement.AvailabilityZone,Name:Tags[?Key==`Name`]|[0].Value}' \
-    --output text
