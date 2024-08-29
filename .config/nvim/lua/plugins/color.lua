7def1f7b09759f2c0833912de9326c5ecfded12e
alexrf45 <fonalex45@gmail.com>
2024-08-28 21:25:43 -0400

    usual

--- .config/nvim/lua/plugins/color.lua
+++ .config/nvim/lua/plugins/color.lua
@@ -12,6 +12,6 @@ return {
     -- Optionally configure and load the colorscheme
     -- directly inside the plugin declaration.
     vim.g.edge_enable_italic = true
-    vim.cmd.colorscheme("base16-irblack")
+    vim.cmd.colorscheme("base16-nord")
   end,
 }
