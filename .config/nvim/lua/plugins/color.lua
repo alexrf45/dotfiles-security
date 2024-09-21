16f294e1906fe64312954b6701c177bb16762bf7
alexrf45 <fonalex45@gmail.com>
2024-09-20 23:03:31 -0400

    alacritty themes

--- .config/nvim/lua/plugins/color.lua
+++ .config/nvim/lua/plugins/color.lua
@@ -1,17 +1,17 @@
--- return {
---   "audibleblink/hackthebox.vim",
---   config = function()
---     vim.cmd([[colorscheme hackthebox]])
---   end,
--- }
 return {
-  "RRethy/base16-nvim",
-  lazy = false,
-  priority = 1000,
+  "rebelot/kanagawa.nvim",
   config = function()
-    -- Optionally configure and load the colorscheme
-    -- directly inside the plugin declaration.
-    vim.g.edge_enable_italic = true
-    vim.cmd.colorscheme("base16-gruvbox-material-dark-hard")
+    vim.cmd([[colorscheme kanagawa-dragon]])
   end,
 }
+-- return {
+--   "RRethy/base16-nvim",
+--   lazy = false,
+--   priority = 1000,
+--   config = function()
+--     -- Optionally configure and load the colorscheme
+--     -- directly inside the plugin declaration.
+--     vim.g.edge_enable_italic = true
+--     vim.cmd.colorscheme("base16-gruvbox-material-dark-hard")
+--   end,
+-- }
