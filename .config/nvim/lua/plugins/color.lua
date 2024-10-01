b61e3e20a5aee4ec0679042bb78101279dd1f758
alexrf45 <fonalex45@gmail.com>
2024-09-30 20:17:14 -0400

    i3status

--- .config/nvim/lua/plugins/color.lua
+++ .config/nvim/lua/plugins/color.lua
@@ -1,17 +1,17 @@
-return {
-  "rebelot/kanagawa.nvim",
-  config = function()
-    vim.cmd([[colorscheme kanagawa-dragon]])
-  end,
-}
 -- return {
---   "RRethy/base16-nvim",
---   lazy = false,
---   priority = 1000,
+--   "rebelot/kanagawa.nvim",
 --   config = function()
---     -- Optionally configure and load the colorscheme
---     -- directly inside the plugin declaration.
---     vim.g.edge_enable_italic = true
---     vim.cmd.colorscheme("base16-gruvbox-material-dark-hard")
+--     vim.cmd([[colorscheme kanagawa-dragon]])
 --   end,
 -- }
+return {
+  "RRethy/base16-nvim",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    -- Optionally configure and load the colorscheme
+    -- directly inside the plugin declaration.
+    vim.g.edge_enable_italic = true
+    vim.cmd.colorscheme("base16-nord")
+  end,
+}
