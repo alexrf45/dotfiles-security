d95e51cc32081d9296b2dea25520c88143140521
alexrf45 <fonalex45@gmail.com>
2024-10-13 10:34:55 -0400

    tmux issue

--- .zsh/tmux_default.sh
+++ .zsh/tmux_default.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 tmux_default() {
-	if command -v tmux &>/dev/null && [ -n "$PS1" ] && [[ ! "$TERM" =~ screen ]] && [[ ! "$TERM" =~ tmux ]] && [ -z "$TMUX" ]; then
-		exec tmuxp load ~/.config/tmuxp/dev.yaml
-	fi
+  if command -v tmux &>/dev/null && [ -n "$PS1" ] && [[ ! "$TERM" =~ screen ]] && [[ ! "$TERM" =~ tmux ]] && [ -z "$TMUX" ]; then
+    exec tmuxp load ~/.config/tmuxp/fr3d.yaml
+  fi
 }
 
 tmux_default
