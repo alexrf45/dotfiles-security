3330386517c2cab34c29a862675a0e12f0a554a3
alexrf45 <fonalex45@gmail.com>
2024-10-18 00:24:27 -0400

    htb

--- .config/nvim/lua/plugins/color.lua
+++ .config/nvim/lua/plugins/color.lua
@@ -4,14 +4,21 @@
 --     vim.cmd([[colorscheme kanagawa-dragon]])
 --   end,
 -- }
+-- return {
+--   "RRethy/base16-nvim",
+--   lazy = false,
+--   priority = 1000,
+--   config = function()
+--     -- Optionally configure and load the colorscheme
+--     -- directly inside the plugin declaration.
+--     vim.g.edge_enable_italic = true
+--     vim.cmd.colorscheme("base16-tender")
+--   end,
+-- }
+
 return {
-  "RRethy/base16-nvim",
-  lazy = false,
-  priority = 1000,
+  "audibleblink/hackthebox.vim",
   config = function()
-    -- Optionally configure and load the colorscheme
-    -- directly inside the plugin declaration.
-    vim.g.edge_enable_italic = true
-    vim.cmd.colorscheme("base16-tender")
+    vim.cmd([[colorscheme hackthebox]])
   end,
 }
