f75cfab5818c7057f90ddc23c1e06bbbe6751b33
alexrf45 <fonalex45@gmail.com>
2024-10-24 17:38:57 -0400

    gruvbox-material-dark-hard

--- .config/nvim/lua/plugins/color.lua
+++ .config/nvim/lua/plugins/color.lua
@@ -1,20 +1,20 @@
-return {
-  "rebelot/kanagawa.nvim",
-  config = function()
-    vim.cmd([[colorscheme kanagawa-dragon]])
-  end,
-}
 -- return {
---   "RRethy/base16-nvim",
---   lazy = false,
---   priority = 1000,
+--   "olimorris/onedarkpro.nvim",
 --   config = function()
---     -- Optionally configure and load the colorscheme
---     -- directly inside the plugin declaration.
---     vim.g.edge_enable_italic = true
---     vim.cmd.colorscheme("base16-tender")
+--     vim.cmd([[colorscheme onedark_dark]])
 --   end,
 -- }
+return {
+  "RRethy/base16-nvim",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    -- Optionally configure and load the colorscheme
+    -- directly inside the plugin declaration.
+    vim.g.edge_enable_italic = true
+    vim.cmd.colorscheme("base16-gruvbox-material-dark-hard")
+  end,
+}
 
 -- return {
 --   "audibleblink/hackthebox.vim",
