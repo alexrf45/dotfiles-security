f00ec3d1ecdf046405fe14ce5b6806d3eb11ddbb
alexrf45 <fonalex45@gmail.com>
2024-11-13 22:58:46 -0500

    markdown

--- .config/nvim/lua/plugins/markdown.lua
+++ .config/nvim/lua/plugins/markdown.lua
@@ -1,17 +1,18 @@
 return {
   -- add symbols-outline
   {
-    -- "ixru/nvim-markdown",
-    -- cmd = "",
-    -- keys = { { "<leader>nn", "<cmd>NoNeckPain<cr>", desc = "[N]o [N]eckpain" } },
-  },
-
-  {
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    ft = { "markdown" },
-    build = function()
-      vim.fn["mkdp#util#install"]()
-    end,
+    "ixru/nvim-markdown",
+    cmd = "",
+    keys = { { "<leader>nn", "<cmd>NoNeckPain<cr>", desc = "[N]o [N]eckpain" } },
   },
 }
+--   {
+--     "iamcco/markdown-preview.nvim",
+--     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+--     build = "cd app && npm install",
+--     init = function()
+--       vim.g.mkdp_filetypes = { "markdown" }
+--     end,
+--     ft = { "markdown" },
+--   },
+-- }
